b582f1bdf50996a9971f37c959fb72d8933b115b
Justin Olah <justinolah@Justins-MacBook-Pro-5.local>
2021-06-02 11:12:04 -0700

    feature extraction first commit

--- feature_extraction.py
+++ feature_extraction.py
@@ -2,18 +2,17 @@ import sklearn
 from sklearn.feature_extraction.text import CountVectorizer
 from analyze import *
 
-def extractFeaturesBagOfWords(data):
-	matrix = CountVectorizer(max_features=4000)
-	X = matrix.fit_transform(data.text).toarray()
+def extractFeaturesBagOfWords(data, ngram):
+	vectorizer = CountVectorizer(max_features=4000, ngram_range=ngram)
+	X = vectorizer.fit_transform(data.text).toarray()
+	#print(vectorizer.vocabulary_)
 	return X
 
 
 def main():
-	data = getData()
-	emotions = getEmotions()
-	filtered_data = getFilteredData()
-	cleanText(filtered_data)
-	X = extractFeaturesBagOfWords(filtered_data)
+	data = getFilteredData()
+	cleanText(data)
+	X = extractFeaturesBagOfWords(data, (1,1))
 
 if __name__ == "__main__":
 	main()
