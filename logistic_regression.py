62ee09a76737ea13882f250ceb50588842c9656d
Justin Olah <justinolah@Justins-MacBook-Pro-5.local>
2021-06-05 18:51:26 -0700

    logistic regression added LIWC lexicon features

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,17 +4,30 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
 from analyze import *
 
 import liwc
 parse, category_names = liwc.load_token_parser('data/LIWC.dic')
 
+#Feature extractor for LIWC Lexicon
+class LIWCFeatureExtractor(BaseEstimator, TransformerMixin):
+
+    def __init__(self):
+        pass
+
+    def transform(self, data, y=None):
+    	return data.apply(getLIWCFeatures).to_list()
+
+    def fit(self, X, y=None):
+        return self
+
 def extractFeaturesBagOfWords(texts, vocab_list):
 	vectorizer = CountVectorizer(max_features=4000, vocabulary=vocab_list, ngram_range=(1,1))
 	X = vectorizer.fit_transform(texts).toarray()
-	print(vectorizer.vocabulary_)
-	print(len(vectorizer.vocabulary_))
+	#print(vectorizer.vocabulary_)
+	#print(len(vectorizer.vocabulary_))
 	return X
 
 def getYVector(labels, numEmotions):
@@ -45,8 +58,6 @@ def main():
 	vocab_list = sorted(word_freq[word_freq > 2].index)
 	print(len(vocab_list))
 
-	X = extractFeaturesBagOfWords(data.text, vocab_list)
-
 	emotions = getEmotions()
 	emotions.remove("neutral")
 
@@ -56,16 +67,18 @@ def main():
 	test = getTestSet()
 	cleanText(test)
 
+	features = FeatureUnion([
+		('liwc', LIWCFeatureExtractor()),
+    	('cvec', CountVectorizer(vocabulary=vocab_list))
+    ])
+
 	logRegPipeline = Pipeline([
-		('cvec', CountVectorizer(vocabulary=vocab_list)),
-		('clf', OneVsRestClassifier(LogisticRegression(max_iter=500, solver='sag'), n_jobs=1)),
+		('feats', features),
+		('clf', OneVsRestClassifier(LogisticRegression(max_iter=1000, solver='sag'), n_jobs=1)),
 	])
 
 	x_train = train.text
 	x_test = test.text
-
-	x_train_liwc = train.text.apply(getLIWCFeatures)
-	x_test_liwc = test.text.apply(getLIWCFeatures)
 
 	y_train = train.labels.apply(lambda x: getYVector(x,len(emotions))).to_list()
 	y_test = test.labels.apply(lambda x: getYVector(x,len(emotions))).to_list()
