f6405880dbf55fc6a82134d11fbcbb48ab404ff5
justinolah <justinolah5@gmail.com>
2021-08-08 15:38:50 -0700

    multiple heads

--- bert_visualize_attention.py
+++ bert_visualize_attention.py
@@ -137,7 +137,7 @@ def main():
 	for i in range(12):
 		head_i = attention[0,i,:length,:length]
 
-		vec = torch.sum(head_ig, dim=0)
+		vec = torch.sum(head_i, dim=0)
 		vec = softmax(vec)
 		vec = vec.detach()
 
